36126230f3549458f1c3be170795e0acabecaa1b
AbhayrajPaul <2k23abhayraj@gmail.com>
2024-02-16 13:08:23 +0530

    ep 02 Igniting our app

--- react - practice/code.js
+++ react - practice/code.js
@@ -1,1 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 
+const wrap = ReactDOM.createRoot(document.querySelector(".wrapper"));
+const h2 = React.createElement("h2", {}, "React 2");
+wrap.render(h2);
